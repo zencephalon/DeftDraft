4fb91d700d01eb5550e0736aca8586ca931ba16d
Matthew Bunday <zenceph@gmail.com>
2013-05-23 00:16:27 -0400

    Massive refactoring, getting ready to add some jquery magic.

--- deftdraft.js
+++ deftdraft.js
@@ -1,41 +1,12 @@
-function setSelectionRange(input, selectionStart, selectionEnd) {
-    if (input.setSelectionRange) {
-        input.focus();
-        input.setSelectionRange(selectionStart, selectionEnd);
-    }
-    else if (input.createTextRange) {
-        var range = input.createTextRange();
-        range.collapse(true);
-        range.moveEnd('character', selectionEnd);
-        range.moveStart('character', selectionStart);
-        range.select();
-    }
-}
-
-function setCaret(input, pos) {
-    setSelectionRange(input, pos, pos);
-}    
-
-function getCaret(el) {
-    if (el.selectionStart) {
-        return el.selectionStart;
-    } else if (document.selection) {
-        el.focus();
-
-        var r = document.selection.createRange();
-        if (r == null) {
-            return 0;
-        }
-
-        var re = el.createTextRange(),
-            rc = re.duplicate();
-        re.moveToBookmark(r.getBookmark());
-        rc.setEndPoint('EndToStart', re);
-
-        return rc.text.length;
-    } 
-    return 0;
-}
+$(document).ready(function() {
+    $('.drafts').cycle({
+		fx: 'scrollHorz', // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+                timeout: 0,
+                next: '#next',
+                prev: '#prev',
+                speed: 500
+	});
+});
 
 // saved buffer
 var sbuffer = new Buffer('', 0);
